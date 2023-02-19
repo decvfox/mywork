8df0aff4f7bfdb3b537071e9ddb873e0468ef22c
decvfox <10021067@atu.ie>
2023-02-19 17:01:44 +0000

    Week04-Flow

---
+++ Weeko4-Flow/lab4.1.1-isEven.py
@@ -0,0 +1,10 @@
+# lab4.1.1-isEven.py
+# Outputs whether a number is odd or even
+#Author: Declan Fox
+
+number = int(input("enter an integer: "))
+
+if (number % 2) == 0:
+    print (f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
