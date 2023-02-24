0a2ebad2e2a291ccf2c1d91fc09da24968555664
decvfox <10021067@atu.ie>
2023-02-24 22:56:22 +0000

    week 5 extra labs

---
+++ week05-Data Structures/lab5.1.5-inputStudentData.py
@@ -0,0 +1,21 @@
+# lab5.1.5-inputStudentData.py
+# Write a program that will read in the data for the data structure in lab5.1.4
+# Author: Declan Fox
+
+modules = []
+course = ' '
+grade = ' '
+student_name = input("please enter a student name: ")
+
+while course != '' or grade != '':
+    course = input('Enter the course name, blank to quit: ')
+    grade = input('Enter a grade, blank to quit: ')
+    modules.append({"courseName": course, "grade": grade})
+
+student = {"name": student_name,
+            "modules": modules}
+
+
+print (f'Student: {student["name"]}')
+for module in student["modules"]:
+    print(f"\t {module['courseName']} \t: {module['grade']}")
---
+++ week05-Data Structures/lab5.1.6-studentDataFunction.py
@@ -0,0 +1,32 @@
+# lab5.1.6-studentDataFunction.py
+# write a function that that will read in the data for the data structure in lab5.1.4
+# Author: Declan Fox
+
+students = []
+
+def student_data():
+    # function to get student's name and grades
+    modules = []
+    course = ' '
+    grade = ' '
+    student_name = input("please enter a student name: ")
+
+    while course != '' or grade != '':
+        course = input('Enter the course name, blank to quit: ')
+        grade = input('Enter a grade, blank to quit: ')
+        modules.append({"courseName": course, "grade": grade})
+
+    student = {"name": student_name,
+                "modules": modules}
+    return student
+
+
+get_more_students = 'y'
+while get_more_students == 'y':
+    students.append(student_data())
+    get_more_students = input("y to continue: ")
+
+for student in students:
+    print (f'Student: {student["name"]}')
+    for module in student["modules"]:
+        print(f"\t {module['courseName']} \t: {module['grade']}")
