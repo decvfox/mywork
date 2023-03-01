6820a78300cecc3cf407f885da6ef1482f4ef519
decvfox <10021067@atu.ie>
2023-03-01 23:05:42 +0000

    week 06 labs

---
+++ Week06-functions/lab6.1.1-yo.py
@@ -0,0 +1,11 @@
+# lab6.1.1-yo.py
+# function yo takes one parameter
+# now it prints it out
+# Author: Declan Fox
+
+
+def yo(a):
+ return a * 2
+# here we are calling the function yo
+# and are passing in an argument, 3
+print(yo(3))
